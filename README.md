7547266580bed50d87b42cff57f7e3239e0b1762
李宗祐 <tp_mini@lizongyous-Mac-Studio.local>
2024-12-03 15:46:35 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RunningmanApp
+1. By episode
+2. Naver map 
+3. tutorial
+4. basic
---
+++ runningman_app/android/app/src/main/kotlin/com/example/runningman_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.runningman_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
